c94c365e929501e0cc36e3d0ae66a67d50a322ec
Hebaadelenan <85418852+Hebaadelenan@users.noreply.github.com>
2023-08-29 05:15:32 +0300

    Update bitwise_operations.h

--- bitwise_operations.h
+++ bitwise_operations.h
@@ -8,9 +8,10 @@
 #ifndef BITWISE_OPERATIONS_H_
 #define BITWISE_OPERATIONS_H_
 
-#define Set_Bit(Register,Bit) (Register = Register | (1<<Bit))
-#define Clear_Bit(Register,Bit) (Register = Register & (~(1<<Bit)) )
-#define Get_Bit(Register,Bit) ( (Register>>Bit)& 1 )
+#define Set_Bit(Register,Bit) ((Register) = (Register) | (1<<(Bit)))
+#define Clear_Bit(Register,Bit) ((Register) = (Register) & (~(1<<(Bit))) )
+#define Get_Bit(Register,Bit) ( ((Register)>>(Bit)) & 1 )
+#define Toggle_Bit(Register,Bit) ((Register) = (Register) ^ (1<<(Bit)))
 
 
 #endif /* BITWISE_OPERATIONS_H_ */
